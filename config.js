93ec23a7104efb97c519a5b3df6a0b3dbbe758e9
SohamJoshi0510 <sohamjoshi20032002@gmail.com>
2024-10-27 13:38:12 +0530

    Set up Cognito

---
+++ config.js
@@ -0,0 +1,11 @@
+// https://main.d1zvrl7l6qjkmf.amplifyapp.com/
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_N8fxyDqMY', // Replace with your User Pool ID
+        userPoolClientId: 'md0rh41ga6n42o5vpilccduna', // Replace with your App Client ID
+        region: 'ap-south-1' // Replace with your region
+    },
+    api: {
+        invokeUrl: '' // Leave blank for now; add API Gateway URL in Step 7
+    }
+};
